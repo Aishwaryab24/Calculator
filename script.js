8a65446ba99207d286a31c3e600cd9b53edb31fe
Aishwarya <aisha24122000@gmail.com>
2023-11-10 18:09:57 +0530

    Calculator

---
+++ script.js
@@ -0,0 +1,36 @@
+
+
+let input=document.getElementsByTagName("input");
+var numberOfButtons=document.querySelectorAll('button').length;
+
+let string="";
+
+for(var i=0; i<numberOfButtons; i++){
+    document.querySelectorAll('button')[i].addEventListener('click', (e) =>{
+
+        if(e.target.innerHTML=="="){
+            string=eval(string);
+            if(Number.isInteger(string)){
+                document.querySelector("input").value=string;
+            }
+            else{
+                document.querySelector("input").value=string.toFixed(3);
+            }
+            
+        }
+        else if(e.target.innerHTML=="RESET"){
+            string="";
+            document.querySelector("input").value=string;
+        }
+        else if(e.target.innerHTML=="DEL"){
+            string=string.substring(0, string.length-1);
+            document.querySelector("input").value=string;
+        }
+        else{
+            //console.log(e.target);
+            string=string+e.target.innerHTML;
+            document.querySelector("input").value=string;
+        }
+        
+    })
+}
